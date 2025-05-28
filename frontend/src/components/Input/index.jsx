6f39297329867688e53d05481cdc615b53e2ab68
silalahi-17 <aguspangondian04@gmail.com>
2025-05-28 15:45:21 +0700

    perbaiki fungsi kode delete

--- frontend/src/components/Input/index.jsx
+++ frontend/src/components/Input/index.jsx
@@ -1,21 +1,27 @@
-const Input = (props) => {
-  if(props.type === 'checkbox') {
+const Input = ({ label, error, ...rest }) => {
+  const inputClass = `form-control ${error ? 'is-invalid' : ''}`;
+
+  if (rest.type === 'checkbox') {
     return (
-      <div>
-        <input className={props.error && 'is-invalid'} {...props} />
-        <label>{props.label}</label>
-        { props.error && props.error.map(err => <p key={err} className="invalid">* {err}</p>)}
+      <div className="form-group mb-3">
+        <input className={error ? 'is-invalid' : ''} {...rest} />
+        <label>{label}</label>
+        {error && error.map((err) => (
+          <p key={err} className="invalid">* {err}</p>
+        ))}
       </div>
-    )
-  }else {
-    return (
-      <div>
-        <label>{props.label}</label>
-        <input className={`form-control ${props.error && 'is-invalid'}`} {...props} />
-        { props.error && props.error.map(err => <p key={err} className="invalid">* {err}</p>)}
-      </div>
-    )
+    );
   }
-}
+
+  return (
+    <div className="form-group mb-3">
+      <label>{label}</label>
+      <input className={inputClass} {...rest} />
+      {error && error.map((err) => (
+        <p key={err} className="invalid">* {err}</p>
+      ))}
+    </div>
+  );
+};
 
 export default Input;
