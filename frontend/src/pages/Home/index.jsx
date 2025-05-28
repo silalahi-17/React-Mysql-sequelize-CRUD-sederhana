f35120695bbe54a0c5f740dc8d1074f44e4f5cac
silalahi-17 <aguspangondian04@gmail.com>
2025-05-28 16:24:35 +0700

    perbaiki kode menampilkan gambar

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -70,10 +70,11 @@ const Home = () => {
                 <td className="border px-4 py-2">{product.quantity}</td>
                 <td className="border px-4 py-2">
                   <img
-                    src={`${import.meta.env.VITE_API_URL}/${product.images}`}
+                    src={`${import.meta.env.VITE_API_URL}/uploads/${product.images.replace(/\\/g, '/')}`}
                     alt={product.name}
                     style={{ width: '100px', height: '100px', objectFit: 'contain', borderRadius: '5px' }}
                   />
+
                 </td>
                 <td className="border px-4 py-2 text-center">
                   <Link to={`/detail/${product.id}`} className="btn btn-sm btn-info me-1">Detail</Link>
