4f5c83ee2ca9ed4f2dfa9e9201be6e62cac4eec3
silalahi-17 <aguspangondian04@gmail.com>
2025-05-29 10:10:53 +0700

    set navigation static

--- express-eduwork/middlewares/multerConfig.js
+++ express-eduwork/middlewares/multerConfig.js
@@ -6,16 +6,7 @@ const storage = multer.diskStorage({
     cb(null, path.join(__dirname, '../uploads'));
   },
   filename: function (req, file, cb) {
-/*************  ✨ Windsurf Command ⭐  *************/
-/**
- * Sets the file name for the uploaded file.
- * 
- * @param {Object} req - The HTTP request object.
- * @param {Object} file - The file object containing file details.
- * @param {Function} cb - The callback function to be called with the file name.
- */
-
-/*******  172fc078-8c47-461a-87ea-1fb1a440b715  *******/    const ext = path.extname(file.originalname);
+    const ext = path.extname(file.originalname);
     const baseName = path.basename(file.originalname, ext).replace(/\s+/g, '-').toLowerCase();
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
     cb(null, `${baseName}-${uniqueSuffix}${ext}`);
